a5c3c402f1b9d3928642f68dff123fd8b5ddea9f
Vicheka Oeun <oeunvicheka95@gmail.com>
2024-01-04 02:59:36 -0500

    initial api fetch

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,27 +1,37 @@
-import { Component } from 'react';
-import { render } from 'react-dom';
+import React, { useState, useEffect } from 'react';
 import Nav from './Nav.jsx';
+import '../style/shop.css';
 
-class Shop extends Component {
-    constructor() {
-        super();
-    }
-    render() {
-        return (
-            <div className='container'>
-                <Nav />
-                <div className='shop-container'>
-                    <div id='shop-header'>
-                        <h1>Products</h1>
-                    </div>
-                    <div id='shop-product'>
-                        <div>Scarf</div>
-                        <div>Gloves</div>
-                        <div>Beanie</div>
-                    </div>
+const Shop = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch('https://fakestoreapi.com/products/category/jewelery')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+            .catch(error => console.error('Error fetching data:', error));
+    }, []);
+
+    return (
+        <div className='container'>
+            <Nav />
+            <div className='shop-container'>
+                <div id='shop-header'>
+                    <h1>Products</h1>
+                </div>
+                <div id='shop-product'>
+                    <ul>{products.map(product => (
+                        <li key={product.id}>
+                            <div>
+                                <p>{product.title}</p>
+                                <p>Price: {product.price}</p>
+                            </div>
+                        </li>
+                    ))}</ul>
                 </div>
             </div>
-        )
-    }
-}
+        </div>
+    );
+};
+
 export default Shop;
