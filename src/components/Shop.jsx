a6ad1d18a0e63d075bf59c7d136dd8b7d526e29a
Vicheka Oeun <oeunvicheka95@gmail.com>
2024-01-04 20:39:19 -0500

    add loading

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -4,12 +4,19 @@ import '../style/shop.css';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         fetch('https://fakestoreapi.com/products/category/jewelery')
             .then(res => res.json())
-            .then(data => setProducts(data))
-            .catch(error => console.error('Error fetching data:', error));
+            .then(data => {
+                setProducts(data);
+                setLoading(false);
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+                setLoading(false);
+            });
     }, []);
 
     return (
@@ -20,19 +27,23 @@ const Shop = () => {
                     <h1>Products</h1>
                 </div>
                 <div id='shop-product'>
-                    <ul>{products.map(product => (
-                        <li key={product.id}>
-                            <div id='product'>
-                                <div id='product-image'><img src={product.image} /></div>
-                                <div id='product-title'><p><i>{product.title}</i></p></div>
-                                <div id='product-price'><p>Price: {product.price}</p></div>
-                                <div id='product-button'>
-                                    <div><button>Add</button></div>
-                                    <div><button>Remove</button></div>
+                    {loading ? (
+                        <p>Loading...</p>
+                    ) : (
+                        <ul>{products.map(product => (
+                            <li key={product.id}>
+                                <div id='product'>
+                                    <div id='product-image'><img src={product.image} /></div>
+                                    <div id='product-title'><p><i>{product.title}</i></p></div>
+                                    <div id='product-price'><p>Price: {product.price}</p></div>
+                                    <div id='product-button'>
+                                        <div><button>Add</button></div>
+                                        <div><button>Remove</button></div>
+                                    </div>
                                 </div>
-                            </div>
-                        </li>
-                    ))}</ul>
+                            </li>
+                        ))}</ul>
+                    )}
                 </div>
             </div>
         </div>
